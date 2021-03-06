4bc0ab0f45546cd6a80568a7b3726694d43be752
KcRaymond <kaseyleigh1978@gmail.com>
2021-03-06 09:48:19 -0700

    first commit app up and running

---
+++ src/profile.js
@@ -0,0 +1,162 @@
+// Change your display name on tha landing display
+const header = {
+  name: "KR",
+};
+const background = {
+  // Options: Snow or Particle
+  type: "Snow",
+};
+// Write a para about yourself here
+// To update your image, go to './styles/images.css'
+const section2title = "About Me";
+const about = {
+  paragraph:
+    "Innovative front-end developer/designer with success designing and executing design projects from start to end for a variety of high-profile clients/brands. Adept at SEO plans and execution for informational and ecommerce websites. Highly skilled in delivering innovative ideas for books, magazines, brochures, and print media. Knowledge of JavaScript, MySQL, Node.js, Express.js, React.js, MongoDB, Command Line, Git, WordPress, Mac OS X operating systems, networking, troubleshooting, HTML5, CSS3, Flexbox layouts, and evolving technologies. Demonstrated organizational, communication, interpersonal, presentation, creative thinking, and problem-solving skills.",
+};
+// Edit your skill and the percentage you know about it
+// To Add a skill, copy any one below and paste it after the last comma
+const skillsBar = [
+  {
+    name: "PhotoShop | Illustrator | Indesign",
+    value: 100,
+  },
+  {
+    name: "Wordpress | WooCommerce",
+    value: 100,
+  },
+  {
+    name: "Adobe XD | Figma",
+    value: 75,
+  },
+  {
+    name: "HTML5",
+    value: 85,
+  },
+  {
+    name: "CSS3",
+    value: 85,
+  },
+  {
+    name: "React",
+    value: 80,
+  },
+  {
+    name: "Javascript",
+    value: 75,
+  },
+
+  {
+    name: "Node",
+    value: 50,
+  },
+  {
+    name: "Mongoose",
+    value: 50,
+  },
+];
+// Edit your projects, its name, your skills used to make it, and the url.
+// You can omit freely anything if you dont have it
+// To Add a Project, copy any one below and paste it after the last comma and increment the id's project number
+const section3Title = "Past Projects";
+const projects = [
+  {
+    // Add image in './styles/images.css' in #project1
+    id: "project1",
+    name: "Project 1",
+    skills: ["HTML, CSS, JS"],
+    url: "https://github.com/kaustubhai",
+  },
+  {
+    // Add image in './styles/images.css' in #project2
+    id: "project2",
+    name: "Project 2",
+    skills: ["HTML, CSS, JS"],
+    url: "https://github.com/kaustubhai",
+  },
+  {
+    // Add image in './styles/images.css' in #project3
+    id: "project3",
+    name: "Project 3",
+    skills: ["HTML, CSS, JS"],
+    url: "https://github.com/kaustubhai",
+  },
+  {
+    // Add image in './styles/images.css' in #project4
+    id: "project4",
+    name: "Project 4",
+    skills: ["HTML, CSS, JS"],
+    url: "https://github.com/kaustubhai",
+  },
+  {
+    // Add image in './styles/images.css' in #project5
+    id: "project5",
+    name: "Project 5",
+    skills: ["HTML, CSS, JS"],
+    url: "https://github.com/kaustubhai",
+  },
+  {
+    // Add image in './styles/images.css' in #project6
+    id: "project6",
+    name: "Project 6",
+    skills: ["HTML, CSS, JS"],
+    url: "https://github.com/kaustubhai",
+  },
+];
+// Edit your Miscellaneous Activities, its name and the url.
+// You can omit freely anything if you dont have it
+// To Add a Activity, copy any one below and paste it after the last comma and increment the id's Miscellaneous number
+const section4Title = "Miscellaneous";
+const miscellaneous = [
+  {
+    // Add image in './styles/images.css' in #misc1
+    id: "misc1",
+    name: "Miscellaneous 1",
+    url: "https://github.com/kaustubhai",
+  },
+  {
+    // Add image in './styles/images.css' in #misc2
+    id: "misc2",
+    name: "Miscellaneous 2",
+    url: "https://github.com/kaustubhai",
+  },
+  {
+    // Add image in './styles/images.css' in #misc3
+    id: "misc3",
+    name: "Miscellaneous 3",
+    url: "https://github.com/kaustubhai",
+  },
+];
+// Contact form text, and Jotforms link
+// To get your own jotform link, go to https://www.jotform.com/
+// If you hacve the link already, paste it in the contactUrl below
+const section5Title = "Get in Touch";
+const contact = {
+  pitch:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris nunc congue nisi vitae.",
+  copyright: "Kaustubh Mishra",
+  contactUrl: "",
+};
+// Paste your respective social media links. You can omit any if you dont have it
+// Upload your resume in your drive, get the shaareable link and paste it in the resume section
+const social = {
+  github: "https://github.com",
+  facebook: "https://facebook.com",
+  linkedin: "https://linkedin.com",
+  instagram: "https://instagram.com",
+  resume: "https://novoresume.com/",
+};
+// Dont change anything here
+export {
+  header,
+  background,
+  about,
+  skillsBar,
+  projects,
+  miscellaneous,
+  contact,
+  social,
+  section2title,
+  section3Title,
+  section4Title,
+  section5Title,
+};
